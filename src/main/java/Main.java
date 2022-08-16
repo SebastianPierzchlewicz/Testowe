a06ce1006040dd2feaf22435839668d15043a9cf
Seba <sebp3107@gmail.com>
2022-08-16 20:22:11 +0200

    Testowy 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testuje import gitem");
         System.out.println("Zmiena 2");
+        System.out.println("Zmiena 3");
     }
 }
